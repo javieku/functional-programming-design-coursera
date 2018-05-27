14f1d08f791f4208d6a33fcd545fa589baeb416e
javieku <javieku@gmail.com>
2018-05-27 13:37:45 +0200

    Implement finding solutions functions

--- week2/streams/src/main/scala/streams/Solver.scala
+++ week2/streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b.isStanding && b.b1 == goal
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,10 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] =
+    for {
+      (nextBlock, nextMove) <- b.legalNeighbors.toStream
+    } yield (nextBlock, nextMove :: history)
 
   /**
    * This function returns the list of neighbors without the block
@@ -36,7 +39,12 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] =
+    for {
+      (block, history) <- neighbors
+      if !explored.contains(block)
+    } yield (block, history)
+
 
   /**
    * The function `from` returns the stream of all possible paths
