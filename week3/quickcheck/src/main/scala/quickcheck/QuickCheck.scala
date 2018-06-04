07120bb45378ad2e9f4a3c8d430347cdd1221c94
javieku <javieku@gmail.com>
2018-06-04 20:34:55 +0200

    Add implementation of the properties for week 3 assigment

--- week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -23,4 +23,59 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("find-min-two") = forAll { (a: Int, b: Int) =>
+    val h = insert(a, insert(b, empty))
+    findMin(h) == Math.min(a, b)
+  }
+
+  property("delete-min") = forAll { (a: Int) =>
+    val h = insert(a, empty)
+    deleteMin(h) == empty
+  }
+
+  property("delete-min-three") = forAll { (a: Int, b: Int, c: Int) =>
+    val h = insert(a, insert(b, insert(c, empty)))
+    deleteMin(deleteMin(h)) == insert(Math.max(a, Math.max(b, c)), empty)
+  }
+
+  property("heap-sort") = forAll { (h: H) =>
+
+    def sort(h: H): List[Int] = {
+      if (isEmpty(h)) {
+        List[Int]()
+      } else {
+        findMin(h) :: sort(deleteMin(h))
+      }
+    }
+
+    def size(h: H): Int = {
+      if (isEmpty(h)) {
+        0
+      } else {
+        size(deleteMin(h)) + 1
+      }
+    }
+
+    val list = sort(h)
+
+    size(h) == list.size
+
+    list.sorted == list
+  }
+
+  property("find-min-with-meld") = forAll { (h1: H, h2: H) =>
+    val meldedH = meld(h1, h2)
+
+    if (isEmpty(h1) && isEmpty(h2)) {
+      true
+    } else if (isEmpty(h1) && !isEmpty(h2)) {
+      findMin(meldedH) == findMin(h2)
+    } else if (isEmpty(h2) && !isEmpty(h1)) {
+      findMin(meldedH) == findMin(h1)
+    } else if (!isEmpty(h2) && !isEmpty(h1)) {
+      findMin(meldedH) == Math.min(findMin(h1), findMin(h2))
+    } else {
+      false
+    }
+  }
 }
