de71b36360f0230ab4e75dcce358704bcebf944f
javieku <javieku@gmail.com>
2018-06-04 20:34:21 +0200

    Add generator for property test with ScalaCheck

--- week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week3/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,15 +1,21 @@
 package quickcheck
 
-import common._
+import java.lang.Math
 
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import org.scalacheck.Prop.{BooleanOperators, forAll}
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(const(empty), for {
+    v <- arbitrary[Int]
+    h <- genHeap
+  } yield insert(v, h)
+  )
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
