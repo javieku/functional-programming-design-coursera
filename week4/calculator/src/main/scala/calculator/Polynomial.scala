d0651d520db17d64ba064d3c5a0b25af75679420
javieku <javieku@gmail.com>
2018-06-10 10:41:06 +0200

    Implement polynomial root calculator using signals

--- week4/calculator/src/main/scala/calculator/Polynomial.scala
+++ week4/calculator/src/main/scala/calculator/Polynomial.scala
@@ -2,12 +2,26 @@ package calculator
 
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+                   c: Signal[Double]): Signal[Double] = {
+    Signal(b() * b() - (4 * a() * c()))
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+                       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    //(-b ± √Δ) / 2a
+
+    val delta = computeDelta(a,b,c)
+    Signal
+    {
+      val x = delta()
+      if (x < 0)
+        Set()
+      else {
+        val root1 = (-b() + Math.sqrt(x)) / (2 * a())
+        val root2 = (-b() - Math.sqrt(x)) / (2 * a())
+        Set(root1, root2)
+      }
+    }
+
   }
 }
